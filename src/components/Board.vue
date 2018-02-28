6c554f106d71f3fa2d64ad9a408e05df3b42d495
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-02-28 21:23:19 +0530

    Board css added
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -9,5 +9,34 @@
 </script>
 
 <style>
+.grid {
+  background-color: #34495e;
+  color: #fff;
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.gameStatus {
+  margin: 0px;
+  padding: 15px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: #f1c40f;
+  color: #fff;    
+  font-size: 1.4em;
+  font-weight: bold;
+}
+
+.statusTurn {
+    background-color: #f1c40f;
+}
+
+.statusWin {
+    background-color: #2ecc71;
+}
+
+.statusDraw {
+    background-color: #9b59b6;
+}
 
 </style>
