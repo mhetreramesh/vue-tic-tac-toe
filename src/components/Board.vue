79bd7dea484cad519224516e5027ab28d1b8b5f5
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-02-28 21:25:57 +0530

    Component base HTML added
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,21 @@
 <template>
-
+    <table class="grid">
+    <tr>
+        <cell name="1"></cell>
+        <cell name="2"></cell>
+        <cell name="3"></cell>
+    </tr>
+    <tr>
+        <cell name="4"></cell>
+        <cell name="5"></cell>
+        <cell name="6"></cell>
+    </tr>
+    <tr>
+        <cell name="7"></cell>
+        <cell name="8"></cell>
+        <cell name="9"></cell>
+    </tr>
+    </table>
 </template>
 
 <script>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,5 @@
 <template>
-
+    <td class="cell">{{ mark }}</td>
 </template>
 
 <script>
