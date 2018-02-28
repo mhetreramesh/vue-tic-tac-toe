b0e307f2bc8fff05eb03ce81f3f492bd1ce89ea6
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-02-28 21:30:17 +0530

    Board default data
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -20,7 +20,36 @@
 
 <script>
     export default {
-        data () {}
+        data () {
+            return {
+                // can be O or X
+                activePlayer: 'O',
+                // maintains the status of the game: turn or win or draw
+                gameStatus: 'turn',
+
+                gameStatusMessage: `O's turn`,
+                // status color is used as background color in the status bar
+                // it can hold the name of either of the following CSS classes
+                // statusTurn (default) is yellow for a turn
+                    // statusWin is green for a win
+                    // statusDraw is purple for a draw
+                gameStatusColor: 'statusTurn',
+                // no. of moves played by both players in a single game (max = 9)
+                moves: 0,
+                // stores the placement of X and O in cells by their cell number
+                cells: {
+                    1: '', 2: '', 3: '',
+                    4: '', 5: '', 6: '',
+                    7: '', 8: '', 9: ''
+                },
+                // contains all (8) possible winning conditions
+                winConditions: [
+                    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+                    [1, 4, 7], [2, 5, 8],    [3, 6, 9], // columns
+                    [1, 5, 9], [3, 5, 7]             // diagonals
+                ],
+            }
+        }
     }
 </script>
 
