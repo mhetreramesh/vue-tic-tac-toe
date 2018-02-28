aecafd3e66d0ccc6014c48d42d517fb7f5fed4b8
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-02-28 21:22:57 +0530

    Frame style added
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -31,30 +31,68 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+body {
+  background-color: #fff;
+  color: #fff;
+  font-family: 'Dosis', Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
 }
 
-h1, h2 {
-  font-weight: normal;
+#app {
+  margin: 0 auto;
+  max-width: 270px;
+  color: #34495e;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+h1 {
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 3em;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.restart {
+  background-color: #e74c3c;
+  color: #fff;
+  border: 0px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  font-family: 'Dosis', Helvetica, sans-serif;
+  font-size: 1.4em;
+  font-weight: bold;
+  margin: 0px;
+  padding: 15px;
+  width: 100%;
 }
 
-a {
-  color: #42b983;
+.restart:hover {
+  background-color: #c0392b;
+  cursor: pointer;
+}
+
+.scoreBoard {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 15px;
+  background-color: #16a085;
+  box-shadow: 10px solid #fff;
+  padding: 20px;
+  overflow-x: none;
+}
+
+.scoreBoard h2 {
+  margin: 0px;
+}
+
+.scoreBoard span {
+  float: right;
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-left: 20px;
 }
 </style>
