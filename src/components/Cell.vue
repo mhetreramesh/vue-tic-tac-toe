dd0550aee2cdb781583661c15ea969f750e449bb
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-03-02 10:23:42 +0530

    Event triggered on click
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,5 @@
 <template>
-    <td class="cell">{{ mark }}</td>
+    <td class="cell" @click="strike">{{ mark }}</td>
 </template>
 
 <script>
@@ -13,6 +13,19 @@
                 // holds either X or O to be displayed in the td
                 mark: ''
             }    
+        },
+        methods: {
+            strike () {
+                if (! this.frozen) {
+                    // gets either X or O from the Grid component
+                    this.mark = this.$parent.activePlayer
+
+                    this.frozen = true
+
+                    // fires an event to notify the Grid component that a mark is placed
+                    Event.$emit('strike', this.name)
+                }
+            }
         }
     }
 </script>
